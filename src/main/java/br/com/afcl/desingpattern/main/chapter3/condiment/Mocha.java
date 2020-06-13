fef78a8f6012d93fe4d32c83744d8dda200aff0f
André Felipe Camargo Leite <andre.leite@gsw.com.br>
2020-06-13 15:17:44 -0300

    Condiments was created

---
+++ src/main/java/br/com/afcl/desingpattern/main/chapter3/condiment/Mocha.java
@@ -0,0 +1,32 @@
+/*
+ * @(#)Mocha.java 1.0 13/06/2020
+ *
+ * Copyright (c) 2019, Embraer. All rights reserved. Embraer S/A
+ * proprietary/confidential. Use is subject to license terms.
+ */
+package br.com.afcl.desingpattern.main.chapter3.condiment;
+
+import br.com.afcl.desingpattern.main.chapter3.beverage.Beverage;
+
+import java.math.BigDecimal;
+
+/**
+ * @author andrelei - André Felipe C. Leite
+ * @version 1.0 13/06/2020
+ */
+public class Mocha extends CondimentDecorator {
+
+	Beverage beverage;
+
+	public Mocha(final Beverage beverage) {
+		this.beverage = beverage;
+	}
+
+	@Override public String getDescription() {
+		return this.beverage.getDescription() + " -> Mocha";
+	}
+
+	@Override public BigDecimal cost() {
+		return this.beverage.cost().add(BigDecimal.valueOf(0.20f));
+	}
+}
